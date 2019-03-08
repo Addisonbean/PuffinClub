ea4641c97529b90d13032dcbefa35890edfabf0e
Addison Bean <addisonbean@gmail.com>
2019-03-08 13:49:49 -0600

    Draw a cute puffin

--- js/game.js
+++ js/game.js
@@ -1,3 +1,16 @@
 ;(function (){
 	'use strict';
+
+	const canvas = document.getElementById('game');
+	const ctx = canvas.getContext('2d');
+
+	canvas.width = 1000;
+	canvas.height = 1000;
+
+	const puffin = new Image();
+	puffin.addEventListener('load', function() {
+		console.log(this);
+		ctx.drawImage(this, 0, 0);
+	});
+	puffin.src = 'img/puffin.jpg';
 }());
